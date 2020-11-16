7bb264c6bc055087f135b5139ff3244362715776
Dorota Rankowska <dorota.rankowska@gmail.com>
2020-11-16 15:00:23 +0100

    added cart and initiated js file

---
+++ main.js
@@ -0,0 +1,27 @@
+let navBtn = document.querySelector('.nav-btn');
+let closeCartBtn = document.querySelector('.close-cart');
+let clearCartBtn = document.querySelector('.clear-cart');
+let cartDOM = document.querySelector('.cart');
+let cartOverlay = document.querySelector('.cart-overlay');
+let cartItems = document.querySelector('.cart-item');
+let cartTotal = document.querySelector('.cart-total');
+let cartContent = document.querySelector('.cart-content');
+let productsDOM = document.querySelector('.products-display');
+
+let cart = [];
+
+class Products {
+
+}
+
+class UI {
+
+}
+
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+});
